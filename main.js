8e04cc6a2036cbeea3d0bec0dd08dce3eead09e9
a20l <aidaytyn@gmail.com>
2021-03-02 15:40:12 -0600

    small practice of JS printing out on the console the future just like a fortune teller :)

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var number_children = 3;
+var spouse = 'Victoria';
+var place="Australia";
+var job_title = 'Software Engineer';
+
+
+var future = 'You will be a ' + job_title + ' in '+ place + ' and you will have '
+                + number_children + ' children with your spouse ' + spouse;
+
+
+document.write(future);
